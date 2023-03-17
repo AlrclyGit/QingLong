2081259e713018e30ee53e9c3030cc7f6b4c6488
AlrclySuzuki <alrcly@foxmail.com>
2023-03-17 19:15:29 +0800

    阿里云盘签到

---
+++ SA_Aliyun_main.js
@@ -0,0 +1,48 @@
+/**
+ * @name SA_Aliyun_main.js
+ * @author Alrcly Suzuki
+ * @version 0.1
+ */
+
+const updateAccesssTokenURL = "https://auth.aliyundrive.com/v2/account/token"
+const signinURL = "https://member.aliyundrive.com/v1/activity/sign_in_list"
+const refreshToeknArry = process.env.aliyunID
+
+import fetch from "node-fetch";
+
+!(async () => {
+    for (const elem of refreshToeknArry) {
+        // 
+        const queryBody = {
+            'grant_type': 'refresh_token',
+            'refresh_token': elem
+        };
+        //使用 refresh_token 更新 access_token
+        fetch(updateAccesssTokenURL, {
+            method: "POST",
+            body: JSON.stringify(queryBody),
+            headers: { 'Content-Type': 'application/json' }
+        }).then((res) => {
+            return res.json()
+        }).then((json) => {
+            let access_token = json.access_token;
+            //签到
+            fetch(signinURL, {
+                method: "POST",
+                body: JSON.stringify(queryBody),
+                headers: { 'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json' }
+            }).then((res) => {
+                return res.json()
+            }).then((json) => {
+                console.log(json);
+            }).catch((err) => {
+                console.log(err)
+            })
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+})().catch((e) => {
+    console.error(`❗️  运行错误！\n${e}`)
+}).finally()
+

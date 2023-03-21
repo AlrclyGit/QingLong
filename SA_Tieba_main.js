ea646d2c33f156bcf77a12b25a3bf6bde2c89e11
AlrclySuzuki <alrcly@foxmail.com>
2023-03-21 10:10:48 +0800

    优化贴吧签到的时间间隔

--- SA_Tieba_main.js
+++ SA_Tieba_main.js
@@ -1,6 +1,6 @@
 /**
  * @new Env("俊介：百度贴吧签到")
- * @cron 0 4 * * * SA_Tieba_main.js
+ * @cron 0 8 * * * SA_Tieba_main.js
 */
 
 const axios = require("axios")
@@ -13,17 +13,17 @@ class Run {
      */
     constructor() {
         // 获取用户所有关注贴吧
-        this.LIKE_URL = "https://tieba.baidu.com/mo/q/newmoindex";
+        this.LIKE_URL = "https://tieba.baidu.com/mo/q/newmoindex"
         // 获取用户的 Tab
-        this.TBS_URL = "http://tieba.baidu.com/dc/common/tbs";
+        this.TBS_URL = "http://tieba.baidu.com/dc/common/tbs"
         // 贴吧签到接口
-        this.SIGN_URL = "http://c.tieba.baidu.com/c/c/forum/sign";
-        // 存储用户所关注的贴吧
-        this.follow = [];
+        this.SIGN_URL = "http://c.tieba.baidu.com/c/c/forum/sign"
+        // 未签到的贴吧列表
+        this.notSignIn = []
         // 签到成功的贴吧列表 
-        this.success = [];
+        this.successSignIn = []
         // 用户所关注的贴吧数量
-        this.followNum = 0;
+        this.signInNum = 0
         // 登录令牌
         this.BDUSS = process.env.BDUSS
         // barkID
@@ -34,94 +34,72 @@ class Run {
      * 主函数
      */
     main() {
-        this.getTbs()
-    }
-
-    /** 
-     * 获取 Tbs 令牌
-     */
-    getTbs() {
         axios.get(this.TBS_URL, {
             headers: {
                 cookie: this.BDUSS
             }
-        }).then((res) => {
+        }).then(res => {
             if (res.data.is_login === 1) {
                 console.log('获取密钥成功')
                 this.tbs = res.data.tbs
-                this.getFollow()
-            } else {
-                console.log(`获取密钥失败 -- ${res.data}`)
-                axios.get(`https://bark.alrcly.com/${this.barkID}/获取 tbs 失败`)
-            }
-        })
-    }
-
-    /** 
-     * 获取贴吧列表
-     */
-    getFollow() {
-        axios.get(this.LIKE_URL, {
-            headers: {
-                cookie: this.BDUSS
-            }
-        }).then((res) => {
-            console.log(`获取贴吧列表成功`)
-            let jsonArray = res.data.data.like_forum
-            this.followNum = jsonArray.length
-            jsonArray.forEach(element => {
-                if (element.is_sign === 0) {
-                    this.follow.push(element.forum_name.replace("+", "%2B"))
-                } else {
-                    this.success.push(element.forum_name)
-                }
-            });
-            console.log(`需要签到的贴吧：${this.follow}`)
-            console.log(`已经签到的贴吧：${this.success}`)
-            this.runSign()
-        })
-    }
-
-    /** 
-     * 签到
-     */
-    runSign() {
-        let flag = 5
-        while (this.success.length < this.followNum && flag > 0) {
-            console.log(`-----第 ${5 - flag + 1} 轮签到开始-----`)
-            console.log(`还剩 ${this.followNum - this.success.length} 个贴吧需要签到`)
-            this.follow.forEach(element => {
-                let rotation = element.replace("%2B", "+");
-                let sign = md5(`kw=${rotation}tbs=${this.tbs}tiebaclient!!!`)
-                console.log('「${rotation}」签到开始')
-                axios.get(this.SIGN_URL, {
+                return axios.get(this.LIKE_URL, {
                     headers: {
                         cookie: this.BDUSS
-                    },
-                    params: {
-                        kw: element,
-                        tbs: this.tbs,
-                        sign: sign
                     }
-                }).then((res) => {
-                    if (res.data.error_code == 0) {
-                        this.success.push(rotation);
-                        console.log(`「${rotation}」签到成功`);
-                        if (this.follow == []) {
-                            console.log('完成所有贴吧签到')
-                        }
-                    } else {
-                        console.log(res.data)
-                        console.log(`「${rotation}」签到失败`);
-                        axios.get(`https://bark.alrcly.com/${this.barkID}/「${rotation}」签到失败`)
-                    }
-                }).catch((e) => {
-                    console.error(`❗️  运行错误！\n${e}`)
-                    axios.get(`https://bark.alrcly.com/${this.barkID}/百度贴吧签到失败 A`)
                 })
-            })
-            flag--
-        }
+            } else {
+                console.log(`获取密钥失败 -- ${res}`)
+                axios.get(`https://bark.alrcly.com/${this.barkID}/获取 tbs 失败`)
+                process.exit()
+            }
+        }).then(res => {
+            console.log(`获取贴吧列表成功`)
+            let likeForum = res.data.data.like_forum
+            this.signInNum = likeForum.length
+            likeForum.forEach(element => {
+                if (element.is_sign === 0) {
+                    this.notSignIn.push(element.forum_name.replace("+", "%2B"))
+                } else {
+                    this.successSignIn.push(element.forum_name)
+                }
+            });
+            console.log(`需要签到的贴吧：${this.notSignIn}`)
+            console.log(`已经签到的贴吧：${this.successSignIn}`)
+            console.log(`还剩 ${this.signInNum - this.successSignIn.length} 个贴吧需要签到`)
+            if (this.notSignIn.length != 0) {
+                console.log(`-----签到开始-----`)
+                for (let i = 0; i < this.notSignIn.length; i++) {
+                    setTimeout(() => {
+                        let rotation = this.notSignIn[i].replace("%2B", "+");
+                        let sign = md5(`kw=${rotation}tbs=${this.tbs}tiebaclient!!!`)
+                        console.log(`「${rotation}」签到开始`)
+                        axios.get(this.SIGN_URL, {
+                            headers: {
+                                cookie: this.BDUSS
+                            },
+                            params: {
+                                kw: this.notSignIn[i],
+                                tbs: this.tbs,
+                                sign: sign
+                            }
+                        }).then((res) => {
+                            if (res.data.error_code == 0) {
+                                this.successSignIn.push(rotation);
+                                console.log(`「${rotation}」签到成功`);
+                                if (this.follow == []) console.log('完成所有贴吧签到')
+                            } else {
+                                console.log(res)
+                                console.log(`「${rotation.data}」签到失败`);
+                                axios.get(`https://bark.alrcly.com/${this.barkID}/「${rotation}」签到失败`)
+                            }
+                        }).catch((e) => {
+                            console.error(`❗️  运行错误！\n${e}`)
+                            axios.get(`https://bark.alrcly.com/${this.barkID}/百度贴吧签到运行错误！`)
+                        })
+                    }, 20000 * i)
+                }
+            }
+        })
     }
 }
 

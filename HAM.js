1f955ab37bb3605ebd282fb4f16ca8968eaab26f
AlrclyForger <Alrcly@Foxmail.com>
2025-05-17 01:47:32 +0800

    ♻️ refactor(HAM): 统一代码格式，移除多余分号

--- HAM.js
+++ HAM.js
@@ -1,16 +1,16 @@
 /**
  * 任务名称
- * name: 俊介：业余无线电考试报名
+ * name: 业余无线电考试报名
  * 定时规则
  * cron: 23 * * * *
  */
 
 //
-const axios = require('axios');
+const axios = require('axios')
 // 登录令牌
-const hamKey = process.env.HAMKEY;
+const hamKey = process.env.HAMKEY
 //
-const getListURL = 'http://82.157.138.16:8091/CRAC/app/exam_exam/getExamList';
+const getListURL = 'http://82.157.138.16:8091/CRAC/app/exam_exam/getExamList'
 
 async function getList() {
   // 构建请求体
@@ -24,25 +24,25 @@ async function getList() {
     req_meta: {
       user_id: hamKey,
     },
-  };
+  }
   const config = {
     headers: {
       'Content-Type': 'application/json', // 设置请求头
     },
-  };
+  }
   try {
-    const res = await axios.post(getListURL, data, config);
+    const res = await axios.post(getListURL, data, config)
     if (res.data.code === 10010) {
-      console.log('湖北业余无线电无考试');
+      console.log('湖北业余无线电无考试')
     } else {
-      console.log('获取到业余无线电考试报名考试安排');
-      QLAPI.notify('获取到业余无线电考试报名考试安排', JSON.stringify(res.data));
+      console.log('获取到业余无线电考试报名考试安排')
+      QLAPI.notify('获取到业余无线电考试报名考试安排', JSON.stringify(res.data))
     }
   } catch (error) {
-    console.error(`❗️  获取数据失败！\n${error}`);
-    QLAPI.notify('业余无线电考试报名监控', '接口请求失败');
+    console.error(`❗️  获取数据失败！\n${error}`)
+    QLAPI.notify('业余无线电考试报名监控', '接口请求失败')
   }
 }
 
 // 调用函数
-getList();
+getList()
